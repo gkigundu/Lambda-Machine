df2db9fcf7d84b09c0bfe9879083df7af4cdad7c
XDerekMaierX <derk.maier@gmail.com>
2016-07-08 22:34:46 -0400

    update

--- alpha/alpha-server2.py
+++ alpha/alpha-server2.py
@@ -15,17 +15,17 @@ import cgitb
 cgitb.enable()
 
 # Global Defaults
-port	= 8000
+port = 8000
 addr = "127.0.0.1"
 codeDir = "codeScrolls"
 
 # get args
 args=sys.argv[1:]
 for i in range(len(args)):
-  if (args[i] == "-p"):
-	port = int(args[i+1])
-  if (args[i] == "-a"):
-	addr = str(args[i+1])
+	if (args[i] == "-p"):
+		port = int(args[i+1])
+	if (args[i] == "-a"):
+		addr = str(args[i+1])
 
 # checks
 if not os.path.exists(codeDir):
@@ -33,46 +33,51 @@ if not os.path.exists(codeDir):
 
 class handler(http.server.BaseHTTPRequestHandler):
 	def set_headers(self, code):
-	  self.send_response(code)
-	  self.send_header(b'Content-type', 'text/html')
-	  self.end_headers()
+		self.send_response(code)
+		self.send_header(b'Content-type', 'text/html')
+		self.end_headers()
 
 	# GET
 	def do_GET(self): # check if contained to directory
-	  filePath=re.sub("^/",os.getcwd()+"/",self.path)
-	  filePath=re.sub("%20"," ",filePath)
-	  filePath=re.sub("/+","/",filePath)
-	  if os.path.isfile(filePath):
-	  	self.set_headers(200)
-		f = open(filePath, 'rb')
-		for i in f:
-		  self.wfile.write(i) # .encode("UTF-8")
-		f.close
-	  elif os.path.isdir(filePath):
-		index=os.path.join(filePath,"index.html")
-		if os.path.isfile(index):
-		  self.set_headers(200)
- 	  	  f = open(index, 'rb')
-		  # does this work??
-		  try:
-		    data = f.read(50)
-			while len(data) > 0:
-			  self.wfile.write(data)
+		filePath=re.sub("^/",os.getcwd()+"/",self.path)
+		filePath=re.sub("%20"," ",filePath)
+		filePath=re.sub("/+","/",filePath)
+		if os.path.isfile(filePath):
+			self.set_headers(200)
+			f = open(filePath, 'rb')
+			try:
+				data = f.read(50)
+				while (len(data) > 0):
+					self.wfile.write(data)
 			finally:
-		      f.close()
-		#   for i in f:
-		#	 self.wfile.write(i) # .encode("UTF-8")
-		#   f.close
+				f.close()
+		if os.path.isdir(filePath):
+			index=os.path.join(filePath,"index.html")
+			if os.path.isfile(index):
+				self.set_headers(200)
+				f = open(index, 'rb')
+				bufferSize=50
+				try:
+					data = f.read(bufferSize)
+					while (len(data) > 0):
+						print(data)
+						self.wfile.write(data)
+						data = f.read(bufferSize)
+				finally:
+					f.close()
+			#	 for i in f:
+			#	 self.wfile.write(i) # .encode("UTF-8")
+			#	 f.close
+			else:
+				for i in os.listdir(filePath):
+					i = "<a href=\'" + self.path + i + "\'>"+i+"</a></br>"
+					self.wfile.write(i.encode("UTF-8"))
+		elif not os.path.exists(filePath):
+			self.set_headers(404)
+			string="File : '" + filePath + "' not found."
+			self.wfile.write(string.encode("UTF-8"))
 		else:
-		  for i in os.listdir(filePath):
-			i = "<a href=\'" + self.path + i + "\'>"+i+"</a></br>"
-			self.wfile.write(i.encode("UTF-8"))
-	  elif not os.path.exists(filePath):
-		self.set_headers(404)
-		string="File : '" + filePath + "' not found."
-		self.wfile.write(string.encode("UTF-8"))
-	  else:
-		self.set_headers(500)
+			self.set_headers(500)
 	# POST
 	def do_POST(self):
 		fp=self.rfile
@@ -92,9 +97,9 @@ class handler(http.server.BaseHTTPRequestHandler):
 			 <head>
 				<meta charset="UTF-8">
 				<script type="text/javascript">
-				   window.location = " '''
+					 window.location = " '''
 		html2=''' ";
-			   </script>
+				 </script>
 			</head>
 			</html>
 		'''
@@ -109,6 +114,6 @@ socketserver.TCPServer.allow_reuse_address = True
 httpd = socketserver.TCPServer((addr, port), handler)
 print("<log> Serving @ " + str(addr) + ":" + str(port))
 try:
-  httpd.serve_forever()
+	httpd.serve_forever()
 except KeyboardInterrupt:
-  sys.exit(0)
+	sys.exit(0)
