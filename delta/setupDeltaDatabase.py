0ade6bf1e79fdfe92bc9f9a3d8e2f94386e651f7
XDerekMaierX <derk.maier@gmail.com>
2016-07-14 19:45:16 -0400

    update

--- delta/setupDeltaDatabase.py
+++ delta/setupDeltaDatabase.py
@@ -12,28 +12,35 @@ urlToGet="https://download.elastic.co/elasticsearch/release/org/elasticsearch/di
 databaseDir="elasticsearch-2.3.4"
 outputZip="master.zip"
 
-try:
-    if not os.path.exists(databaseDir):
-        os.makedirs(databaseDir)
-        req = urllib.request.urlopen(urlToGet)
-        lu.log("Downloading elastic.")
-        with open(outputZip, 'wb') as output:
-            while True:
-                data = req.read(4096)
-                if data:
-                    output.write(data)
-                else:
-                    break
-        lu.log("Unzipping elastic.")
-        with zipfile.ZipFile(outputZip,"r") as zip_ref:
-            zip_ref.extractall(".")
-        lu.log("Removing zip file.")
-        os.remove(outputZip)
-        lu.log("Set up elastic sucessfully.")
-    lu.log("Starting elastic.")
-    lu.log("=================")
-    command = "bash " + databaseDir + "/bin/elasticsearch"
-    command = command.split(" ")
-    subprocess.run(command , stdout=sys.stdout, stderr=sys.stderr)
-except IOError as e:
-    lu.error("Write error", e)
+def main():
+    try:
+        if not os.path.exists(databaseDir):
+            os.makedirs(databaseDir)
+            req = urllib.request.urlopen(urlToGet)
+            lu.log("Downloading elastic.")
+            with open(outputZip, 'wb') as output:
+                while True:
+                    data = req.read(4096)
+                    if data:
+                        output.write(data)
+                    else:
+                        break
+            lu.log("Unzipping elastic.")
+            with zipfile.ZipFile(outputZip,"r") as zip_ref:
+                zip_ref.extractall(".")
+            lu.log("Removing zip file.")
+            os.remove(outputZip)
+            lu.log("Set up elastic sucessfully.")
+        lu.log("Starting elastic.")
+        lu.log("=================")
+
+        command = "bash " + databaseDir + "/bin/elasticsearch"
+        command = command.split(" ")
+        proc = subprocess.Popen(command , stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        # run server here to respond to http get requests concerning proc
+        while (1):
+            for line in iter(proc.stdout.readline,''):
+                print(line.decode("utf-8").rstrip() )
+    except IOError as e:
+        lu.error("Write error", e)
+main()
